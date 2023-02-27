d037e558623867eecc3ffd023bdb296587556680
dafuga <danielfugere28@gmail.com>
2023-02-27 18:06:20 -0500

    fix: returning signatures on sign
    issue #6

--- src/esr.ts
+++ src/esr.ts
@@ -2,8 +2,13 @@ import {CallbackPayload, Signature} from '@wharfkit/session'
 
 export function extractSignaturesFromCallback(payload: CallbackPayload): Signature[] {
     const signatures: Signature[] = []
+
     while (payload[`sig${signatures.length}`]) {
         signatures.push(Signature.from(payload[`sig${signatures.length}`]!))
+    }
+
+    if (signatures.length === 0) {
+        signatures.push(Signature.from(payload.sig))
     }
 
     return signatures
